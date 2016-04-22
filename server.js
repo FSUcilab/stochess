f098bfdf0c1a9cf883f13db73d8751e859ed7a5f
Nathan Crock <mathnathan@gmail.com>
2016-04-22 00:55:07 +0000

    Restructured the code to be more like a normal web app. Rewrote the node js server using express

--- server.js
+++ server.js
@@ -1,48 +1,31 @@
-var http = require('http'),
-    url = require('url'),
-    path = require('path'),
-    fs = require('fs');
-var mimeTypes = {
-    "html": "text/html",
-    "jpeg": "image/jpeg",
-    "jpg": "image/jpeg",
-    "png": "image/png",
-    "js": "text/javascript",
-    "css": "text/css"};
-    
-http.createServer(function(req, res) {
-    var uri = url.parse(req.url).pathname;
-    var filename = path.join(process.cwd(), unescape(uri));
-    var stats;
+var express = require('express');
 
-    try {
-        stats = fs.lstatSync(filename); // throws if path doesn't exist
-    } catch (e) {
-        res.writeHead(404, {'Content-Type': 'text/plain'});
-        res.write('404 Not Found\n');
-        res.end();
-        return;
-    }
+var queryLi = require('./js/queryLi.js');
 
-    if (stats.isDirectory()) filename += '/index.html';
+var app = express();
 
-    var mimeType = mimeTypes[path.extname(filename).split(".").reverse()[0]];
+app.use(express.static('js'));
+app.use('/js', express.static(__dirname + '/js'));
 
-    fs.readFile(filename, "binary", function(err, file) {
-        if(err) {        
-            res.writeHead(500, {"Content-Type": "text/plain"});
-            res.write(err + "\n");
-            res.end();
-            return;
-        }
+app.get('/', function(req, res, next) {
+  //index
+  console.log("hiii")
+  res.send("hiiii")
+})
+app.get('/queryli', function(req, res, next) {
+  // enable CORS
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
-        res.writeHead(200, {'Content-Type': mimeType} );
-        res.write(file, "binary");
-        res.end();
-    });
-}).listen(8888);
+  var query = req.query;
+  console.log("query!", req.query)
+  res.send("ok!")
+})
 
-console.log("Static file server running at\n  => http://localhost:8888/\nCTRL + C to shutdown");
-
+ var port = 8888;
+var server = app.listen(port, function () {
+  var host = 'localhost'
+  console.log('query server listening at http://%s:%s', host, port);
+});
 
 
