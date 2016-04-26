29e1436ac07c452910ce0660042bd2b03b7faa52
Nathan Crock <mathnathan@gmail.com>
2016-04-26 21:05:28 +0000

    Another big update. Created new dedicated training server for the computer vs computer. Also added a sampling selection method

--- src/tools.js
+++ src/tools.js
@@ -1,9 +1,174 @@
-var request = require('request');
-var qs = require('qs');
-var async = require('async');
-var chessjs = require('../js/chess.min.js');
+var request = require('request'),
+    qs = require('qs'),
+    async = require('async'),
+    crypto = require('crypto'),
+    chessjs = require('../js/chess.min.js');
 
 var exports = module.exports = {};
+
+function simulate(startFen, collection, numSims, callback) {
+
+  console.log("\n---Simulating...");
+  async.times(numSims, function(simNum, callback2) {
+    var game = new chessjs.Chess(startFen);
+    var winner = 'draws';
+    var material = startFen.split(" ")[0].match(/[a-z]/gi).length;
+    var moves = 0;
+    while (!game.game_over() && material > 6){
+      var possibleMoves = game.moves();
+      //play random move
+      var randInt = crypto.randomBytes(1).readUInt8(0);
+      var randomMoveIdx = Math.floor(randInt/256 * possibleMoves.length);
+      var moveToPlay = possibleMoves[randomMoveIdx];
+      //console.log("SIM: ", simNum, ", randNum = ", randInt, ", randomMoveIdx = ", randomMoveIdx, ", move = ", moveToPlay);
+      var move = game.move(moveToPlay);
+      if(move.captured) material -= 1;
+      moves += 1;
+    }
+    console.log("Random simulation took ", moves, " moves...");
+
+    if (game.in_checkmate()) {
+        console.log("ended in checkmate!");
+        var loser = game.turn();
+        //if white lost, then black won.
+        //otherwise, white won
+        loser == 'white' ? winner = 'black' : winner = 'white';
+        callback2(null, winner);
+    } else {
+      console.log("game not in checkmate!");
+      console.log("Looking for record in local database");
+      collection.find({"fen": game.fen()}).toArray(function (err, result) {
+        if (err) console.log("Error!: ", err);
+        else {
+          if (result.length) {
+            console.log("Endgame found in database...");
+            console.log("The winner is... ", result[0].outcome);
+            callback2(err, result[0].outcome);
+          } else {
+            console.log("Endgame not found in database...");
+            console.log("Querying online tablebase...");
+            queryEndGame(game.fen(), function(error, gameResult) {
+              console.log("The best move is... ", gameResult);
+              console.log("Inserting endgame record into local database...");
+              collection.insert(gameResult, function(err, report) {
+                if (err) console.log("Error inserting new record into database: ", err);
+                else console.log(report);
+                callback2(err, gameResult.outcome);
+              });
+            });
+          }
+        }
+      });
+    }
+  }, function(err, results) {
+      var simStats = {'fen': startFen, 'white': 0, 'draws': 0, 'black': 0};
+      async.each(results, function(result, callback3) {
+        simStats[result] += 1;
+        callback3(null);
+      }, function(err) {
+        callback(null, simStats);
+      });
+  });
+};
+
+exports.simulate = simulate;
+
+function formProbabilities(moveStats, turn) {
+
+  var totGames = 0;
+  var g = [];
+  for(var i=0, len=moveStats.length; i < len; i++) {
+    s = moveStats[i];
+    g.push(s.white + s.draws + s.black);
+    totGames += g[i];
+  }
+
+  var totScaledWins = 0;
+  p = [];
+  for(var i=0, len=g.length; i < len; i++) {
+    s = moveStats[i];
+    if(turn === 'w') {
+      var wins = s.white;
+    } else {
+      var wins = s.black;
+    }
+    var scaledWins = (g[i]/totGames)*(2*wins + s.draws);
+    p.push( scaledWins );
+    totScaledWins += scaledWins;
+  }
+
+  for(var i=0, len=p.length; i < len; i++) {
+    p[i] /= totScaledWins;
+  }
+
+  console.log("Probabilities = ", p);
+
+  return p;
+}
+
+exports.formProbabilities = formProbabilities;
+
+function chooseMoveCompetition(moveStats, probabilities) {
+
+  console.log("In chooseMoveComptetition");
+  console.log("probabilities = ", probabilities);
+  if (probabilities.length === 0) {
+    console.log("No available moves!");
+    return -1;
+  }
+
+  var max = probabilities[0];
+  var maxIndex = 0;
+
+  for (var i = 1; i < probabilities.length; i++) {
+    if (probabilities[i] > max) {
+      maxIndex = i;
+      max = probabilities[i];
+    }
+  }
+
+  console.log("maxIndex = ", maxIndex);
+  console.log("max = ", max);
+  console.log("Number of moves choosing from = ", moveStats.length);
+  return moveStats[maxIndex].fen;
+
+}
+
+exports.chooseMoveCompetition = chooseMoveCompetition;
+
+function chooseMoveTraining(moveStats, probabilities) {
+
+  console.log("In chooseMoveTraining");
+  console.log("probabilities = ", probabilities);
+  var numMoves =  probabilities.length;
+  console.log("Number of moves choosing from = ", numMoves);
+  if (numMoves === 0) {
+    console.log("No available moves!");
+    return -1;
+  }
+
+  var max = 0;
+  for(var i=0; i<numMoves; i++) {
+    if (probabilities[i] > max) max = probabilities[i];
+  }
+    
+  console.log("Maximum Probability = ", max);
+
+  var moveIndex = 0;
+  while( !moveIndex ) {
+    var randIndex = Math.floor(crypto.randomBytes(1).readUInt8(0)/256 * numMoves);
+    var randProb = crypto.randomBytes(1).readUInt8(0)/255; 
+    console.log("randIndex = ", randIndex, ", randProb = ", randProb, ", prob[randIndex] = ", probabilities[randIndex]);
+    if( probabilities[randIndex] >= randProb ) {
+      moveIndex = randIndex;
+    }
+  }
+  console.log("Sampled move = ", moveStats[moveIndex].fen);
+
+  return moveStats[moveIndex].fen;
+}
+
+exports.chooseMoveTraining = chooseMoveTraining;
 
 function pgnToFen(pgn, callback){
 
@@ -45,6 +210,9 @@ function queryEndGame(fen, callback) {
 
   request.get(query,
     function (error, response, body) {
+      console.log("error = ", error);
+      console.log("response = ", response);
+      console.log("body = ", body);
       var json = JSON.parse(body);
       if(error) {
         console.log("Error: ", error);
